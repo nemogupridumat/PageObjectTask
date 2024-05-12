f0152ad8e3b21016a24355665921fa7e08ca3d97
alinabu <alinabukhmetova@yandex.ru>
2024-05-12 15:59:50 +0500

    refactored tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.CalculatorPage;
 import pages.CloudGoogleComPage;
-import pages.SearchResultsPage;
 import pages.YOPMailPage;
 
 import java.util.Set;
@@ -23,18 +22,11 @@ public class CalculatorTest {
 
     @Test
     @Order(1)
-    public void performTheSearchTest() {
+    public void checkTotalEstimatedCostAfterCalculationTest() {
         CloudGoogleComPage cloudGoogleComPage = new CloudGoogleComPage(driver);
         cloudGoogleComPage.open();
         cloudGoogleComPage.searchForCalculator("Google Cloud Platform Pricing Calculator");
 
-        SearchResultsPage searchResultsPage = new SearchResultsPage(driver);
-        searchResultsPage.clickCalculatorSearchResult();
-    }
-
-    @Test
-    @Order(2)
-    public void googleCloudPricingCalculatorTest() {
         CalculatorPage calculatorPage = new CalculatorPage(driver);
         calculatorPage.fillForm("4", "leave blank");
         calculatorPage.clickAddToEstimateButton();
@@ -46,20 +38,15 @@ public class CalculatorTest {
     }
 
     @Test
-    @Order(3)
-    public void yopMailTest() {
+    @Order(2)
+    public void checkTotalEstimatedCostInEmailTest() {
         YOPMailPage yopMailPage = new YOPMailPage(driver, wait);
 
         yopMailPage.open("https://yopmail.com/");
 
         yopMailPage.generateEmail();
-        //yopMailPage.clickCloseAdButton();
         yopMailPage.copyEmail();
-    }
 
-    @Test
-    @Order(4)
-    public void checkResultsTest() {
         Set<String> windowHandles = driver.getWindowHandles();
         String firstTabHandle = windowHandles.toArray()[0].toString();
         String secondTabHandle = windowHandles.toArray()[1].toString();
@@ -70,13 +57,11 @@ public class CalculatorTest {
         calculatorPage.inputEmail();
 
         driver.switchTo().window(secondTabHandle);
-        YOPMailPage yopMailPage = new YOPMailPage(driver, wait);
         yopMailPage.checkMail();
 
         yopMailPage.refreshMail();
         String total = "Total Estimated Cost: USD 1,081.20 per 1 month";
         Assertions.assertTrue(total.contains(yopMailPage.getTotalCost()));
-
     }
 
     @AfterAll
